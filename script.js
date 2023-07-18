c01bbf4cbb85430f22865239b07d99ced35ea9e6
Devis <104310438+Deviss12@users.noreply.github.com>
2023-07-18 19:44:44 +0200

    Add game functionality
    Input data is compared to computer's choice
    
    if input is not equal to rock, paper or scissors
    return information that input is invalid
    
    if input is equal to computer's choice return
    information that the game ended with draw
    
    if input beats computer's choice return
    information that player wins, string contains
    what player and computer chose
    
    if input loses to computer's choice return
    according information

--- script.js
+++ script.js
@@ -5,8 +5,38 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {}
-
-const playerSelection = "Rock";
+const playerSelection = prompt(
+  "Make your choice! Rock, Paper or Scissors?"
+).toLowerCase();
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `It's a draw!`;
+  } else if (
+    playerSelection !== "rock" &&
+    playerSelection !== "paper" &&
+    playerSelection !== "scissors"
+  ) {
+    return "Invalid input!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `Player Wins! ${
+      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+    } beats ${
+      computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+    }!`;
+  } else {
+    return `You lose! ${
+      computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+    } beats ${
+      playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+    }!`;
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+console.log(playerSelection, computerSelection);

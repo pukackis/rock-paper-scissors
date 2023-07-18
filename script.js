6f2688756c2ead39544e6e4a20016e045a8ffad6
Devis <104310438+Deviss12@users.noreply.github.com>
2023-07-18 20:44:23 +0200

    Add for loop to iterate function 5 times
    To initate game write game(playRound)
    game will then initate 5 times, each time showing the results

--- script.js
+++ script.js
@@ -5,38 +5,51 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-const playerSelection = prompt(
-  "Make your choice! Rock, Paper or Scissors?"
-).toLowerCase();
-const computerSelection = getComputerChoice();
+// const playerSelection = prompt(
+//   "Make your choice! Rock, Paper or Scissors?"
+// ).toLowerCase();
+// const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return `It's a draw!`;
+    return `It's a draw!`; // Informs about draw
   } else if (
     playerSelection !== "rock" &&
     playerSelection !== "paper" &&
     playerSelection !== "scissors"
   ) {
-    return "Invalid input!";
+    return "Invalid input!"; // informs about invalid input by player
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `Player Wins! ${
+    return `You win! ${
       playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
     } beats ${
       computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-    }!`;
+    }!`; // informs who won the game
   } else {
     return `You lose! ${
       computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
     } beats ${
       playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-    }!`;
+    }!`; // informs who won the game
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerSelection, computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+// console.log(playerSelection, computerSelection);
+
+function game(playRound) {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt(
+      "Make your choice! Rock, Paper or Scissors?"
+    ).toLowerCase();
+    let computerSelection = getComputerChoice();
+
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerSelection, computerSelection);
+  }
+}

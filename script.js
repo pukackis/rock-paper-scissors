3c4ba72b6cbc9133490c8fc076b08c8681731fdc
Devis <104310438+Deviss12@users.noreply.github.com>
2023-08-04 17:17:03 +0200

    Add final functionality, score tracking, show final result, add reset button

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function getComputerChoice() {
 } // Function to generate computer choice each round
 
 let playerSelection = ""; //Starting point for user selection
+let playing; // Determines if the game is over
+
+const init = function () {
+  player = 0;
+  computer = 0;
+  scorePlayer.textContent = 0;
+  scoreComputer.textContent = 0;
+  whoWon.textContent = "";
+  playing = true;
+};
 
 const rockButton = document
   .querySelector(".rock")
@@ -19,12 +29,21 @@ const scissorsButton = document
   .querySelector(".scissors")
   .addEventListener("click", () => startGame("scissors"));
 
+const restartButton = document
+  .querySelector(".restart")
+  .addEventListener("click", init);
 // Event listeneners for game buttons
 
 const scorePlayer = document.querySelector(".current1");
 const scoreComputer = document.querySelector(".current2");
+const whoWon = document.querySelector(".who_won");
 
 // Query selectors for score tracking
+
+let player = 0;
+let computer = 0;
+
+// Starting points for game scores
 
 function startGame(selection) {
   playerSelection = selection;
@@ -39,44 +58,47 @@ function startGame(selection) {
 //Function that starts the game and retrieves result from playRound()
 
 function updateRoundStatus(message) {
-  const whoWon = document.querySelector(".who_won");
   whoWon.textContent = message;
 }
 
 //Function that retrieves who won the round, used to update roundWinner
-
-let player = 0;
-let computer = 0;
-
-// Starting points for game scores
-
+init();
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    updateRoundStatus(`It's a draw!`); // Informs about draw
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    player++;
-    console.log(player, computer);
-    updateRoundStatus(
-      `You win! ${
-        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-      } beats ${
-        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-      }!`
-    ); // informs who won the game
-  } else {
-    computer++;
-    console.log(player, computer);
-    updateRoundStatus(
-      `You lose! ${
-        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-      } beats ${
-        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-      }!`
-    ); // informs who won the game
+  if (playing) {
+    if (playerSelection === computerSelection) {
+      updateRoundStatus(`It's a draw!`); // Informs about draw
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      player++;
+      console.log(player, computer);
+      updateRoundStatus(
+        `You win! ${
+          playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+        } beats ${
+          computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+        }!`
+      ); // informs who won the game
+    } else {
+      computer++;
+      console.log(player, computer);
+      updateRoundStatus(
+        `You lose! ${
+          computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+        } beats ${
+          playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+        }!`
+      ); // informs who won the game
+    }
+    if (player === 5) {
+      updateRoundStatus(`You win the game with computer`);
+      playing = false;
+    } else if (computer === 5) {
+      updateRoundStatus(`You lose the game with computer`);
+      playing = false;
+    }
   }
 }
 
